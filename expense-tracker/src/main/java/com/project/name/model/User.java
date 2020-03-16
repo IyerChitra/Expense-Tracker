eabc37b82c3410b3e8a881d278336e5e39987f24
Chitra Iyer <iyerchitra93@gmail.com>
2020-03-16 21:13:14 +0530

    Initial Commit of scaffolding

---
+++ expense-tracker/src/main/java/com/project/name/model/User.java
@@ -0,0 +1,44 @@
+package com.project.name.model;
+
+
+public class User {
+	
+	private String firstName;
+    private String lastName;
+    private String emailId;
+    private int mobileNo;
+    private String walletId;
+
+    public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public int getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(int mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getWalletId() {
+		return walletId;
+	}
+	public void setWalletId(String walletId) {
+		this.walletId = walletId;
+	}
+	    
+    }
+
---
+++ expense-tracker/src/main/java/com/project/name/model/Wallet.java
@@ -0,0 +1,52 @@
+package com.project.name.model;
+
+import java.util.Date;
+
+public class Wallet {
+	private String walletId;
+    private String walletName;
+    private int amount;
+    private String createdBy;
+    private Date createdTime;
+    private String status;
+    
+	public String getWalletId() {
+		return walletId;
+	}
+	public void setWalletId(String walletId) {
+		this.walletId = walletId;
+	}
+	public String getWalletName() {
+		return walletName;
+	}
+	public void setWalletName(String walletName) {
+		this.walletName = walletName;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedTime() {
+		return createdTime;
+	}
+	public void setCreatedTime(Date createdTime) {
+		this.createdTime = createdTime;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+    
+    
+
+}
